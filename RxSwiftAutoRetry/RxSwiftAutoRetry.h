8319318900133e15e592ceb1bcd06efeb62c20af
Krystian Bujak <kbujak421@gmail.com>
2018-11-21 14:36:22 +0100

    Initial Commit

---
+++ RxSwiftAutoRetry/RxSwiftAutoRetry.h
@@ -0,0 +1,19 @@
+//
+//  RxSwiftAutoRetry.h
+//  RxSwiftAutoRetry
+//
+//  Created by Krystian Bujak on 21/11/2018.
+//  Copyright © 2018 SwingDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxSwiftAutoRetry.
+FOUNDATION_EXPORT double RxSwiftAutoRetryVersionNumber;
+
+//! Project version string for RxSwiftAutoRetry.
+FOUNDATION_EXPORT const unsigned char RxSwiftAutoRetryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSwiftAutoRetry/PublicHeader.h>
+
+
